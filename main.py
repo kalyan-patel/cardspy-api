58c88e95af6835fefd2ebcdc9d056d7510601300
kalyan-patel <kalpat314@gmail.com>
2025-12-13 03:41:33 -0500

    cors

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from card_spy import get_last_deck
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # fine for now, restrict later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class DeckRequest(BaseModel):
     player: str
